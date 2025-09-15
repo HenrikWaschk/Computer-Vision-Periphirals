5e9d65864d5b1f5cd8884725caaf7cf45612602c
henrik <waschk.henrik@gmail.com>
2025-09-15 22:50:20 +0200

    Little tweak

--- Handtracking.py
+++ Handtracking.py
@@ -17,7 +17,7 @@ recognizer = vision.GestureRecognizer.create_from_options(recognizer_options)
 
 class HandTracker:
     HAND_COUNT = Handcount  # maximum number of hands to track
-    def __init__(self, model_complexity=Model_complexity, detection_conf=0.5, tracking_conf=0.5):
+    def __init__(self, model_complexity=Model_complexity, detection_conf=0.5, tracking_conf=0.3):
         self.mp_hands = mp.solutions.hands
         self.mp_drawing = mp.solutions.drawing_utils
         self.hands = self.mp_hands.Hands(
@@ -50,9 +50,15 @@ class HandTracker:
         gesture_result = recognizer.recognize(mp_image)
 
         if results.multi_hand_landmarks:
-            for hand_index, (hand_landmarks, handedness,hand_gestures) in enumerate(
-                zip(results.multi_hand_landmarks, results.multi_handedness,gesture_result.gestures)
-            ):
+            num_hands = len(results.multi_hand_landmarks)
+            for hand_index in range(self.HAND_COUNT):
+                if hand_index >= num_hands:
+                    break
+                hand_landmarks = results.multi_hand_landmarks[hand_index]
+                handedness = results.multi_handedness[hand_index]
+
+                hand_gestures = gesture_result.gestures[hand_index] if len(gesture_result.gestures) > hand_index else []
+
                 label = handedness.classification[0].label
                 score = handedness.classification[0].score
                 if EMA_Smoothing:
